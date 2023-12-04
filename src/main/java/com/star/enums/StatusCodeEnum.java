54829af17a670b3ae5e35335477d0c3780dc5e98
william <1502318069@qq.com>
2023-12-04 11:59:58 +0800

    feature: 状态枚举更改

--- src/main/java/com/star/enums/StatusCodeEnum.java
+++ src/main/java/com/star/enums/StatusCodeEnum.java
@@ -7,25 +7,25 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum StatusCodeEnum {
 
-    SUCCESS(20000, "操作成功"),
+    SUCCESS(200, "操作成功"),
 
-    NO_LOGIN(40001, "用户未登录"),
+    NO_LOGIN(401, "用户未登录"),
 
-    AUTHORIZED(40300, "没有操作权限"),
+    AUTHORIZED(403, "没有操作权限"),
 
-    SYSTEM_ERROR(50000, "系统异常"),
+    SYSTEM_ERROR(500, "系统异常"),
 
-    FAIL(51000, "操作失败"),
+    FAIL(501, "操作失败"),
 
-    VALID_ERROR(52000, "参数格式不正确"),
+    VALID_ERROR(502, "参数格式不正确"),
 
-    USERNAME_EXIST(52001, "用户名已存在"),
+    USERNAME_EXIST(521, "用户名已存在"),
 
-    USERNAME_NOT_EXIST(52002, "用户名不存在"),
+    USERNAME_NOT_EXIST(522, "用户名不存在"),
 
-    ARTICLE_ACCESS_FAIL(52003, "文章密码认证未通过"),
+    ARTICLE_ACCESS_FAIL(523, "文章密码认证未通过"),
 
-    QQ_LOGIN_ERROR(53001, "qq登录错误");
+    QQ_LOGIN_ERROR(531, "qq登录错误");
 
     private final Integer code;
 
